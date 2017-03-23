e23a5acdb9a543081ce356812661626eaed86daf
Arindam Sarkar -X (arinsark - TATA CONSULTANCY SERVICES LIMITED at Cisco) <arinsark@partnet.cisco.com>
2017-03-23 14:24:54 +0530

    arinsark 23-03-2017 2:24pm

--- Connections/config.php
+++ Connections/config.php
@@ -4,15 +4,15 @@
 
 /***************Prod DB Details ***************************/
 
- $hostname_shop = "us-cdbr-iron-east-03.cleardb.net";
+/*  $hostname_shop = "us-cdbr-iron-east-03.cleardb.net";
  $database_shop = "heroku_813d276cc465e94";
  $username_shop = "b1162df0501833";
- $password_shop = "6423989b";
+ $password_shop = "6423989b"; */
 
 /********************Dev DB Details ********************************/
-// $hostname_shop = "localhost";
-// $database_shop = "hotinddeals_db";
-// $username_shop = "root";
-// $password_shop = "admin";
+$hostname_shop = "localhost";
+$database_shop = "hotinddeals_db";
+$username_shop = "root";
+$password_shop = "admin";
 
 ?>
