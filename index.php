8950a9c2f2d481d1be043aed7dfcf9242fe706ea
Arindam Sarkar -X (arinsark - TATA CONSULTANCY SERVICES LIMITED at Cisco) <arinsark@partnet.cisco.com>
2017-08-08 13:15:46 +0530

    arinsark commit 08-08-2017 1-15pm

--- index.php
+++ index.php
@@ -109,6 +109,18 @@ $totalRows_Recordset5 = mysql_num_rows($Recordset5);
       }, "fast");
    });
  </script>
+ 
+ <!-- Hotjar Tracking Code for www.freekaamaals.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:586351,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
 <body>
 
