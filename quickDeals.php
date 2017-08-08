08553ba7ba33c47f867e7a3d86455570c1b81f70
Arindam Sarkar -X (arinsark - TATA CONSULTANCY SERVICES LIMITED at Cisco) <arinsark@partnet.cisco.com>
2017-08-08 14:11:25 +0530

    arinsark commit 08-08-2017 2-12pm

--- quickDeals.php
+++ quickDeals.php
@@ -9,7 +9,7 @@
               // Execute query
               $result = mysql_query($sql,$shop);
               // Loop through each records 
-			 
+			  $i=1;
               while($row = mysql_fetch_array($result))
               {
                $Id=$row['CategoryId'];
@@ -17,11 +17,15 @@
                $Description=$row['Description'];
                $Image=$row['Image'];
 			   $MainCategoryNext=$row['MainCategoryName'];
+			   if($i<=5)
+			   {
             ?>
-							
-							<li><a href="products.php?CategoryId=<?php echo $Id;?>"><?php echo $CategoryName?></a></li>
+						
+				<li><a href="products.php?CategoryId=<?php echo $Id;?>"><?php echo $CategoryName?></a></li>
 																
 			<?php
+			   }
+			   $i=$i+1;
             }
             // Retrieve Number of records returned
             $records = mysql_num_rows($result);
